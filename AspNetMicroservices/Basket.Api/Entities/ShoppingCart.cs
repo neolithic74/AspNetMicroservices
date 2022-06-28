9c9377f4b87f8ba6896740ffbc1fad0de7a37384
Marco Brancaleoni <marco.brancaleoni@4servicesolution.it>
2022-06-28 17:22:41 +0200

    Implement Basket

---
+++ AspNetMicroservices/Basket.Api/Entities/ShoppingCart.cs
@@ -0,0 +1,18 @@
+﻿namespace Basket.Api.Entities
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+
+        } 
+
+        public ShoppingCart(string Username)
+        {
+            UserName = Username;
+        }
+
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+    }
+}
